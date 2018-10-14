b7c1ac3b65a497fac58faf189cf2c81f667356ca
Miranda Xu <mirandaxuuu@users.noreply.github.com>
2018-10-14 12:29:45 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'TCWH5ArLXSStjefsiRxlbvMSR',
-  consumer_secret:      'NBtc3AiMLuUSDD7Fb2fPYcpCU6UD5qL5vAHbALbQWoEqQVb1DM',
-  access_token:         '3134522964-qaakTsXZOx8GOvUZMOTBsjAeteYk6KhVsDEjoe1',
-  access_token_secret:  'MlGB6oJCTaTeVZAhQsYnW2tVgKPMDqin9NeMCu8TwPITP',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
